cabe9c89204fb7c31bb187a303331e28e4833dd2
Jonas Bakelaar <jbakelaa@fred.socs.uoguelph.ca>
2018-04-08 16:42:55 -0400

    Final commit

--- fleschindex.adb
+++ fleschindex.adb
@@ -1,9 +1,9 @@
 -- Author: Jonas Bakelaar (0964977)
--- Date: March 4, 2018
--- Description: Program that takes a jumble of letters and prints out the anagrams of that jumble of letters based on the small dictionary provided with Linux
--- Sources: I used the algorithm at this link: https://www.geeksforgeeks.org/write-a-c-program-to-print-all-permutations-of-a-given-string/ 
---          This algorithm helped me find all the permutations of the letter jumble the user provides. Some additional modifications were required to use the algorithm in ADA. 
---          As a result this is not a direct copy of the code in the link, just a reference used to figure out how to get the permutations of a string efficiently.
+-- Date: April 8, 2018
+-- Description: Program that calculates the Flesch index and Flesch Kincaid Level of a text passage provided through a file.
+-- Sources: I used the algorithm at this link: http://www.csc.villanova.edu/~mdamian/Past/csc3990fa08/papers/nlp/talburt_1986.pdf 
+--          This algorithm helped set a base for how to count syllables, words and sentences. It also provided a base code/algorithm for how to determine the 
+--          flesch index of a particular passage.
 
 with Ada.Text_IO; use Ada.Text_IO;
 with ada.Integer_Text_IO; use Ada.Integer_Text_IO;
@@ -11,7 +11,7 @@ with ada.strings.unbounded; use ada.strings.unbounded;
 with ada.strings.unbounded.Text_IO; use ada.strings.unbounded.Text_IO;
 with ada.characters.handling; use ada.characters.handling;
 with ada.Float_Text_IO; use ada.Float_Text_IO;
-procedure fleschindex is
+procedure fleschIndex is
     fileName: unbounded_string;
     line: unbounded_string;
     sentenceCount: integer := 0;
@@ -26,7 +26,7 @@ procedure fleschindex is
     
     begin
     
-        Put_Line("Input file name");
+        Put_Line("Input file name (or Q to quit)");
         get_line(fileName);
     
     end getFileName;
@@ -38,9 +38,6 @@ procedure fleschindex is
         stringLength := length(line);
         loop
             if(Element(line, i) = '.' or Element(line, i) = ';' or Element(line, i) = '?' or Element(line, i) = '!' or Element(line, i) = ':') then
-                put("Element ");
-                put(Element(line, i));
-                put(" has caused the sentence count to go up!");
                 sentenceCount := sentenceCount + 1;
             end if;
             i := i + 1;
@@ -64,7 +61,7 @@ procedure fleschindex is
                 if(Element(line, i) = ' ' and Element(line, i+1) /= ' ') then
                     -- If there's a space and no space immediately after, add 1 to wordCount
                     vowelCount := vowelCount + 1;
-                elsif(Element(line, i) /= ' ' and (Element(line, i+1) = '.' or Element(line, i+1) = ';' or Element(line, i+1) = '?' or Element(line, i+1) = '!' or Element(line, i+1) = ':'  or (i+2 > stringLength))) then
+                elsif(Element(line, i) /= ' ' and (i+2 > stringLength)) then
                     -- If it's the end of a sentence, add to word count
                     vowelCount := vowelCount + 1;
                 end if;
@@ -78,54 +75,57 @@ procedure fleschindex is
         
         --Find more syllables
         loop
-            put_line("i value: " & integer'image(i));
-            if(i+1 <= stringLength) then
+            --put_line("i value: " & integer'image(i));
+            if(i <= stringLength) then
 	            if (Element(line, i) = ' ') then
-	                j := i + 1;
+	                j := i+1;
 	                loop
-	                    if(j+1 <= stringLength) then
-		                    if (Element(line, j) = 'a' or Element(line, j) = 'A'
-			                 or Element(line, j) = 'e' or Element(line, j) = 'E' 
-			                 or Element(line, j) = 'i' or Element(line, j) = 'I'
-			                 or Element(line, j) = 'o' or Element(line, j) = 'O'
-			                 or Element(line, j) = 'u' or Element(line, j) = 'U'
-			                 or Element(line, j) = 'y' or Element(line, j) = 'Y') then
-			                    if(
-			                     (Element(line, j) = 'e' or Element(line, j) = 'E')
-			                     and 
-			                     (Element(line, j+1) = ',' or Element(line, j+1) = ' ' or Element(line, j+1) = '.' or Element(line, j+1) = ';' or Element(line, j+1) = '?' or Element(line, j+1) = '!' or Element(line, j+1) = ':'  or (j+1 > stringLength))) then
-			                        --Do nothing
-			                        doNothing := true;
-			                    elsif(Element(line, j+1) = 'a' or Element(line, j+1) = 'A'
-			                     or Element(line, j+1) = 'e' or Element(line, j+1) = 'E' 
-			                     or Element(line, j+1) = 'i' or Element(line, j+1) = 'I'
-			                     or Element(line, j+1) = 'o' or Element(line, j+1) = 'O'
-			                     or Element(line, j+1) = 'u' or Element(line, j+1) = 'U'
-			                     or Element(line, j+1) = 'y' or Element(line, j+1) = 'Y') then
-			                        --Do nothing
-			                        doNothing := true;
-			                    elsif(j+2 <= stringLength) then
-			                        --checks with j+2 (e.g. check for final es and ed
-			                        if((Element(line, j) = 'e' or Element(line, j) = 'E')
-			                        and
-			                          (Element(line, j+1) = 'd' or Element(line, j+1) = 'D'
-			                            or Element(line, j+1) = 's' or Element(line, j+1) = 'S')
-			                        and
-			                          (Element(line, j+2) = ' ' or j+2 > stringLength
-			                            or Element(line, j+2) = ',' or Element(line, j+2) = '.' or Element(line, j+2) = ';' or Element(line, j+2) = '?' or Element(line, j+2) = '!' or Element(line, j+2) = ':')) then
-			                            --Do nothing
-			                            doNothing := true;
-			                        end if;
+		                if (Element(line, j) = 'a' or Element(line, j) = 'A'
+			             or Element(line, j) = 'e' or Element(line, j) = 'E' 
+			             or Element(line, j) = 'i' or Element(line, j) = 'I'
+			             or Element(line, j) = 'o' or Element(line, j) = 'O'
+			             or Element(line, j) = 'u' or Element(line, j) = 'U'
+			             or Element(line, j) = 'y' or Element(line, j) = 'Y') then
+			                 if(j+1 <= stringLength) then
+				                if(
+				                 (Element(line, j) = 'e' or Element(line, j) = 'E')
+				                 and 
+				                 (Element(line, j+1) = ',' or Element(line, j+1) = ' ' or Element(line, j+1) = '.' or Element(line, j+1) = ';' or Element(line, j+1) = '?' or Element(line, j+1) = '!' or Element(line, j+1) = ':'  or (j+1 > stringLength))
+				                 and
+				                 (Element(line, j-1) /= 'L' and Element(line, j-1) /= 'l')) then
+				                    --Do nothing
+				                    --put_line("Do nothing flag triggered with " & element(line, j) & " element at i: " & element(line, i));
+				                    doNothing := true;
+				                elsif(Element(line, j+1) = 'a' or Element(line, j+1) = 'A'
+				                 or Element(line, j+1) = 'e' or Element(line, j+1) = 'E' 
+				                 or Element(line, j+1) = 'i' or Element(line, j+1) = 'I'
+				                 or Element(line, j+1) = 'o' or Element(line, j+1) = 'O'
+				                 or Element(line, j+1) = 'u' or Element(line, j+1) = 'U'
+				                 or Element(line, j+1) = 'y' or Element(line, j+1) = 'Y') then
+				                    --Do nothing
+				                    doNothing := true;
+				                elsif(j+2 <= stringLength) then
+				                    --checks with j+2 (e.g. check for final es and ed
+				                    if((Element(line, j) = 'e' or Element(line, j) = 'E')
+				                    and
+				                      (Element(line, j+1) = 'd' or Element(line, j+1) = 'D'
+				                        or Element(line, j+1) = 's' or Element(line, j+1) = 'S')
+				                    and
+				                      (Element(line, j+2) = ' ' or j+2 > stringLength
+				                        or Element(line, j+2) = ',' or Element(line, j+2) = '.' or Element(line, j+2) = ';' or Element(line, j+2) = '?' or Element(line, j+2) = '!' or Element(line, j+2) = ':')) then
+				                        --Do nothing
+				                        doNothing := true;
+				                    end if;
+				                end if;
+				                --Can add to the vowel count
+				                if(doNothing = False) then
+			                        vowelFlag := vowelFlag + 1;
 			                    end if;
-			                    --Can add to the vowel count
-			                    if(doNothing = False) then
-		                            vowelFlag := vowelFlag + 1;
-		                        end if;
 		                    end if;
 	                    end if;
-	                    j := j + 1;
 	                    doNothing := false;
-	                    exit when Element(line, j) = ' ' or j+1 > stringLength;
+	                    exit when Element(line, j) = '-' or Element(line, j) = ' ' or Element(line, j) = ',' or Element(line, j) = '.' or Element(line, j) = ';' or Element(line, j) = '?' or Element(line, j) = '!' or Element(line, j) = ':' or j >= stringLength;
+	                    j := j + 1;
 	                end loop;
 	            end if;
 	            
@@ -135,10 +135,9 @@ procedure fleschindex is
 	            end if;
 	            vowelFlag := -1;
 	        end if;
+            exit when i >= stringLength;
 	        i := i+1;
-            exit when i > stringLength+1;
         end loop;
-        put_line("Syllable count:" & integer'image(vowelCount));
         
     end getVowelCount;
     
@@ -176,7 +175,7 @@ procedure fleschindex is
         --Calculate it
         wordDivSentence := (Float(wordCount)/Float(SentenceCount));
         vowelDivWord := (Float(vowelCount)/Float(wordCount));
-        put_line("206.835 - ((1.015 * (" & Integer'Image(wordCount) & "/" & Integer'Image(sentenceCount) &")) + (84.6 * ("& Integer'Image(vowelCount) &"/"& Integer'Image(wordCount) &"))");
+        --put_line("206.835 - ((1.015 * (" & Integer'Image(wordCount) & "/" & Integer'Image(sentenceCount) &")) + (84.6 * ("& Integer'Image(vowelCount) &"/"& Integer'Image(wordCount) &"))");
         fleschIndexFinal := (206.835 - ((1.015 * (wordDivSentence)) + (84.6 * (vowelDivWord))));
         
     end calculateFleschIndex;
@@ -203,7 +202,7 @@ procedure fleschindex is
         --Perform calculation
         wordDivSentence := (Float(wordCountTemp)/Float(SentenceCountTemp));
         vowelDivWord := (Float(vowelCountTemp)/Float(wordCountTemp));
-        put_line("((0.39*(" & integer'Image(wordCountTemp) & "/"& integer'Image(sentenceCountTemp) & ")+(11.8*(" & integer'image(vowelCountTemp) & "/" & integer'image(wordCountTemp) & ")))-15.59)" );
+        --put_line("((0.39*(" & integer'Image(wordCountTemp) & "/"& integer'Image(sentenceCountTemp) & ")+(11.8*(" & integer'image(vowelCountTemp) & "/" & integer'image(wordCountTemp) & ")))-15.59)" );
         r := ((0.39*(wordDivSentence)+(11.8*(vowelDivWord)))-15.59);
         return r;
     end calculateFleschKincaidLevel;
@@ -218,26 +217,37 @@ procedure fleschindex is
             -- process each line from the file
             get_line(infp,line);
             getSentenceCount(sentenceCount,line);
-            getVowelCount(vowelCount, line);
             getWordCount(wordCount, line);
-            -- process the line of text
+            getVowelCount(vowelCount, line);
         end loop;
         close(infp);
         calculateFleschIndex(sentenceCount, vowelCount, wordCount, fleschIndexFinal);
+        put_line(" ");
+        put_Line("----The Results----");
         put_line("Sentence Count: " & Integer'Image(sentenceCount));
         put_line("Syllable Count: " & Integer'Image(vowelCount));
         put_line("Word count: " & Integer'Image(wordCount));
-        put_line("FleschIndex: " & Integer'Image(Integer(fleschIndexFinal)));
+        put("Flesch Index: ");
+        put(fleschIndexFinal, Fore => 2, Aft => 2, Exp => 0);
+        put_line(" ");
         put("Flesch Kincaid Level: ");
         put(calculateFleschKincaidLevel(sentenceCount, wordCount, vowelCount), Fore => 2, Aft =>2, Exp =>0);
+        put_line(" ");
+        put_line(" ");
+        sentenceCount := 0;
+        wordCount := 0;
+        vowelCount := 0;
     end readFile;
 
 begin
-
-    -- Input the file name
-    getFileName(fileName);
- 
-    -- Call buildLEXICON, which will build the dictionary of words to find anagrams in and populate the array of dictionary words.
-    readFile(fileName);
-    
-end fleschindex;
+    loop
+        -- Input the file name
+        getFileName(fileName);
+        
+        -- If user enters "Q", quit the program
+        exit when fileName = "Q";
+        
+        -- Call the main program functionality which reads the file and determines the Flesch Index and Flesch Kincaid Level.
+        readFile(fileName);
+    end loop;
+end fleschIndex;
